95f36bfa2f04cf13393e4940155033ce1d20e2a5
Eugene Bekker <ebekker@powervision.com>
2017-02-22 06:37:40 -0500

    Passing audit format unit tests

---
+++ main/test/Zyborg.Vault.Core-tests/Audit/FormatTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Zyborg.Vault.Helper.Salt;
+
+namespace Zyborg.Vault.Audit
+{
+	[TestClass]
+	public class FormatTests
+	{
+		//~ type noopFormatWriter struct {
+		//~ }
+		class noopFormatWriter : IAuditFormatWriter
+		{
+			//~ func (n *noopFormatWriter) WriteRequest(_ io.Writer, _ *AuditRequestEntry) error {
+			//~ 	return nil
+			//~ }
+			public void WriteRequest(Stream s, AuditRequestEntry entry)
+			{ }
+
+			//~ func(n* noopFormatWriter) WriteResponse(_ io.Writer, _* AuditResponseEntry) error {
+			//~ 	return nil
+			//~ }
+			public void WriteResponse(Stream s, AuditResponseEntry entry)
+			{ }
+		}
+
+		[TestMethod]
+		//~ func TestFormatRequestErrors(t *testing.T) {
+		public void TestFormatRequestErrors()
+		{
+			//~ salter, _ := salt.NewSalt(nil, nil)
+			//~ config := FormatterConfig{
+			//~ 	Salt: salter,
+			//~ }
+			//~ formatter := AuditFormatter{
+			//~ 	AuditFormatWriter: &noopFormatWriter{},
+			//~ }
+			var salter = Salt.NewSalt(null, null);
+			var config = new FormatterConfig
+			{
+				Salt = salter,
+			};
+			var formatter = new AuditFormatter(new noopFormatWriter());
+
+			//~ if err := formatter.FormatRequest(ioutil.Discard, config, nil, nil, nil); err == nil {
+			//~ 	t.Fatal("expected error due to nil request")
+			//~ }
+			//~ if err := formatter.FormatRequest(nil, config, nil, &logical.Request{}, nil); err == nil {
+			//~ 	t.Fatal("expected error due to nil writer")
+			//~ }
+			Assert.ThrowsException<ArgumentNullException>(() =>
+				formatter.FormatRequest(Stream.Null, config, null, null, null));
+			Assert.ThrowsException<ArgumentNullException>(() =>
+				formatter.FormatRequest(null, config, null, new Logical.Request(), null));
+		}
+
+		[TestMethod]
+		//~ func TestFormatResponseErrors(t *testing.T) {
+		public void TestFormatResponseErrors()
+		{
+			//~ salter, _ := salt.NewSalt(nil, nil)
+			//~ config := FormatterConfig{
+			//~ 	Salt: salter,
+			//~ }
+			//~ formatter := AuditFormatter{
+			//~ 	AuditFormatWriter: &noopFormatWriter{},
+			//~ }
+			var salter = Salt.NewSalt(null, null);
+			var config = new FormatterConfig
+			{
+				Salt = salter,
+			};
+			var formatter = new AuditFormatter(new noopFormatWriter());
+
+			//~ if err := formatter.FormatResponse(ioutil.Discard, config, nil, nil, nil, nil); err == nil {
+			//~ 	t.Fatal("expected error due to nil request")
+			//~ }
+			//~ if err := formatter.FormatResponse(nil, config, nil, &logical.Request{}, nil, nil); err == nil {
+			//~ 	t.Fatal("expected error due to nil writer")
+			//~ }
+			Assert.ThrowsException<ArgumentNullException>(() =>
+				formatter.FormatResponse(Stream.Null, config, null, null, null, null));
+			Assert.ThrowsException<ArgumentNullException>(() =>
+				formatter.FormatResponse(null, config, null, new Logical.Request(), null, null));
+		}
+	}
+}
---
+++ main/test/Zyborg.Vault.Core-tests/Audit/JsonFormatTests.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Zyborg.Util;
+using Zyborg.Util.Collections;
+using Zyborg.Vault.Helper.Salt;
+
+namespace Zyborg.Vault.Audit
+{
+	[TestClass]
+    public class JsonFormatTests
+    {
+		public const string testFormatJSONReqBasicStr = @"{""time"":""2015-08-05T13:45:46Z"",""type"":""request"",""auth"":{""display_name"":"""",""policies"":[""root""],""metadata"":null},""request"":{""operation"":""update"",""path"":""/foo"",""data"":null,""wrap_ttl"":60,""remote_address"":""127.0.0.1"",""headers"":{""foo"":[""bar""]}},""error"":""this is an error""}
+";
+
+		[TestMethod]
+		//~ func TestFormatJSON_formatRequest(t *testing.T) {
+		public void TestFormatJSON_formatRequest()
+		{
+			//~ cases := map[string]struct {
+			//~ 	Auth   *logical.Auth
+			//~ 	Req    *logical.Request
+			//~ 	Err    error
+			//~ 	Prefix string
+			//~ 	Result string
+			//~ }{
+			var cases = new OrderedDictionary<string, (
+					Logical.Auth auth,
+					Logical.Request req,
+					Exception err,
+					string prefix,
+					string result
+				)>
+			{
+				//~ "auth, request": {
+				//~ 	&logical.Auth{ClientToken: "foo", Policies: []string{"root"}},
+				//~ 	&logical.Request{
+				//~ 		Operation: logical.UpdateOperation,
+				//~ 		Path:      "/foo",
+				//~ 		Connection: &logical.Connection{
+				//~ 			RemoteAddr: "127.0.0.1",
+				//~ 		},
+				//~ 		WrapInfo: &logical.RequestWrapInfo{
+				//~ 			TTL: 60 * time.Second,
+				//~ 		},
+				//~ 		Headers: map[string][]string{
+				//~ 			"foo": []string{"bar"},
+				//~ 		},
+				//~ 	},
+				//~ 	errors.New("this is an error"),
+				//~ 	"",
+				//~ 	testFormatJSONReqBasicStr,
+				//~ },
+				["auth, request"] = (
+					new Logical.Auth { ClientToken = "foo", Policies = new string[] { "root" } },
+					new Logical.Request
+					{
+						Operation = Logical.Operation.UpdateOperation,
+						Path = "/foo",
+						Connection = new Logical.Connection
+						{
+							RemoteAddr = "127.0.0.1",
+						},
+						WrapInfo = new Logical.RequestWrapInfo
+						{
+							TTL = TimeSpan.FromSeconds(60),
+						},
+						Headers = new Dictionary<string, string[]>
+						{
+							["foo"] = new string[] { "bar" },
+						},
+					},
+					new Exception("this is an error"),
+					"",
+					testFormatJSONReqBasicStr
+				),
+				//~ "auth, request with prefix": {
+				//~ 	&logical.Auth{ClientToken: "foo", Policies: []string{"root"}},
+				//~ 	&logical.Request{
+				//~ 		Operation: logical.UpdateOperation,
+				//~ 		Path:      "/foo",
+				//~ 		Connection: &logical.Connection{
+				//~ 			RemoteAddr: "127.0.0.1",
+				//~ 		},
+				//~ 		WrapInfo: &logical.RequestWrapInfo{
+				//~ 			TTL: 60 * time.Second,
+				//~ 		},
+				//~ 		Headers: map[string][]string{
+				//~ 			"foo": []string{"bar"},
+				//~ 		},
+				//~ 	},
+				//~ 	errors.New("this is an error"),
+				//~ 	"@cee: ",
+				//~ 	testFormatJSONReqBasicStr,
+				//~ },
+				["auth, request with prefix"] = (
+					new Logical.Auth { ClientToken = "foo", Policies = new string[] { "root" } },
+					new Logical.Request
+					{
+						Operation = Logical.Operation.UpdateOperation,
+						Path = "/foo",
+						Connection = new Logical.Connection
+						{
+							RemoteAddr = "127.0.0.1",
+						},
+						WrapInfo = new Logical.RequestWrapInfo
+						{
+							TTL = TimeSpan.FromSeconds(60),
+						},
+						Headers = new Dictionary<string, string[]>
+						{
+							["foo"] = new string[] { "bar" },
+						},
+					},
+					new Exception("this is an error"),
+					"@cee: ",
+					testFormatJSONReqBasicStr
+				),
+			};
+
+			//~ for name, tc := range cases {
+			foreach (var kv in cases)
+			{
+				var name = kv.Key;
+				var tc = kv.Value;
+
+				//~ var buf bytes.Buffer
+				//~ formatter := AuditFormatter{
+				//~ 	AuditFormatWriter: &JSONFormatWriter{
+				//~ 		Prefix: tc.Prefix,
+				//~ 	},
+				//~ }
+				//~ salter, _ := salt.NewSalt(nil, nil)
+				//~ config := FormatterConfig{
+				//~ 	Salt: salter,
+				//~ }
+				//~ if err := formatter.FormatRequest(&buf, config, tc.Auth, tc.Req, tc.Err); err != nil {
+				//~ 	t.Fatalf("bad: %s\nerr: %s", name, err)
+				//~ }
+				byte[] buf;
+				var formatter = new AuditFormatter(new JsonFormatWriter
+				{
+					Prefix = tc.prefix,
+				});
+				var salter = Salt.NewSalt(null, null);
+				var config = new FormatterConfig
+				{
+					Salt = salter,
+				};
+				using (var ms = new MemoryStream())
+				{
+					formatter.FormatRequest(ms, config, tc.auth, tc.req, tc.err);
+					buf = ms.ToArray();
+				}
+
+				//~ if !strings.HasPrefix(buf.String(), tc.Prefix) {
+				//~ 	t.Fatalf("no prefix: %s \n log: %s\nprefix: %s", name, tc.Result, tc.Prefix)
+				//~ }
+				Assert.IsTrue(buf.ToUtf8String().StartsWith(tc.prefix));
+
+				//~ var expectedjson = new(AuditRequestEntry)
+				//~ if err := jsonutil.DecodeJSON([]byte(tc.Result), &expectedjson); err != nil {
+				//~ 	t.Fatalf("bad json: %s", err)
+				//~ }
+				var expectedjson = JsonConvert.DeserializeObject<AuditRequestEntry>(tc.result);
+
+				//~ var actualjson = new(AuditRequestEntry)
+				//~ if err := jsonutil.DecodeJSON([]byte(buf.String())[len(tc.Prefix):], &actualjson); err != nil {
+				//~ 	t.Fatalf("bad json: %s", err)
+				//~ }
+				var actualjson = JsonConvert.DeserializeObject<AuditRequestEntry>(buf.ToUtf8String().Substring(tc.prefix.Length));
+
+				//~ expectedjson.Time = actualjson.Time
+				expectedjson.Time = actualjson.Time;
+
+				//~ expectedBytes, err := json.Marshal(expectedjson)
+				//~ if err != nil {
+				//~ 	t.Fatalf("unable to marshal json: %s", err)
+				//~ }
+				var expectedBytes = JsonConvert.SerializeObject(expectedjson);
+
+				//~ if !strings.HasSuffix(strings.TrimSpace(buf.String()), string(expectedBytes)) {
+				//~ 	t.Fatalf(
+				//~ 		"bad: %s\nResult:\n\n'%s'\n\nExpected:\n\n'%s'",
+				//~ 		name, buf.String(), string(expectedBytes))
+				//~ }
+				Assert.IsTrue(buf.ToUtf8String().Trim().EndsWith(expectedBytes));
+			}
+		}
+
+    }
+}
